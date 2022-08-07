c7b35566e48235f349dcc46b11355025c5363b58
coldsqurel <57537086+coldsqurel@users.noreply.github.com>
2022-08-07 17:56:12 +0400

    Initial commit

---
+++ MO.py
@@ -0,0 +1,8 @@
+﻿import xlwings as xw
+app = xw.App(visible=True, add_book=False)
+wb = app.books.open(r"C:\Users\alexandrovn\Desktop\otchet\МО2022 таб.xlsm")
+vba_macro = xw.macro("Workbook_Open")
+vba_macro()
+wb.save()
+wb.close()
+app.quit()
---
+++ SVOD.py
@@ -0,0 +1,8 @@
+﻿import xlwings as xw
+app = xw.App(visible=True, add_book=False)
+wb = app.books.open(r"G:\Новое ОКК и УР\Мониторинг\Август 2022\СВОД Август.xlsm")
+vba_macro = wb.macro("Macros")
+vba_macro()
+wb.save()
+wb.close()
+app.quit()
